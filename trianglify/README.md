0f0158fd59dfaa7e24afb7ba286f438def97e9d2
milolu <765232099@qq.com>
2016-06-21 14:12:54 +0800

    trianglify代码调整

---
+++ trianglify/README.md
@@ -0,0 +1,7 @@
+# tringlify
+
+ * 图片视差滚动插件 依赖jq，该插件在jq之后引入
+ * 其他网站实例 http://www.lexus-int.com/magazine/issue8/road/
+ * $('#test1').imgParallax();
+ * $('#test1').imgParallax(options);
+ * 暂不支持class选择器形式 $('.img-box').imgParallax();
---
+++ trianglify/gulpfile.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var browserify = require('browserify');
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var sass = require('gulp-sass');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+var uglify = require('gulp-uglify');
+var buffer = require('vinyl-buffer');
+var source = require('vinyl-source-stream');
+var bourbon = require("bourbon");
+var nodeSass = require('node-sass').includePaths = [bourbon];
+
+
+var inJs = 'app.js';
+var outJs = 'app.min.js';
+
+
+var bundler = browserify('./src/' + inJs, {
+  fullPaths: true
+});
+bundler.exclude('crypto');
+
+gulp.task('browserify', ['jshint'], function() {
+  return bundler.bundle()
+    .on('error', function(error) {
+      console.log(error.message);
+      this.emit('end');
+    })
+    .pipe(source('./' + outJs))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+});
+
+//不做压缩
+gulp.task('browserify-no-uglify', ['jshint'], function() {
+  return bundler.bundle()
+    .on('error', function(error) {
+      console.log(error.message);
+      this.emit('end');
+    })
+    .pipe(source('./' + outJs))
+    .pipe(buffer())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('jshint', function() {
+	return gulp.src(['src/**/*.js'])
+		.pipe(jshint())
+		.pipe(jshint.reporter('jshint-stylish'))
+});
+
+
+gulp.task('sass', function () {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass({
+      outputStyle: 'compressed',
+        includePaths : [ bourbon.includePaths ]
+      })
+    .on('error', sass.logError))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('watch', ['browserify-no-uglify', 'sass'], function() {
+  gulp.watch('src/**/*.js', ['browserify-no-uglify']);
+  gulp.watch('scss/**/*.scss', ['sass']);
+});
+
+gulp.task('clean', function() {
+	return gulp.src('./dist')
+		.pipe(clean());
+});
+
+gulp.task('default', ['browserify', 'sass']);
